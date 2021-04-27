e57ce1ef363aac67456172f95a92c7c031ceb2cc
Luke Fountain <stlfountain@gmail.com>
2021-04-27 12:41:53 -0700

    added daily challenge 6

--- Daily_Challenge_000.js
+++ Daily_Challenge_000.js
@@ -1,1 +1,3 @@
 //This is my template file
+
+//Daily_Challenge_000.js
---
+++ Daily_Challenge_6.js
@@ -0,0 +1,10 @@
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+
+
+// var TempleStrings = function(obj, feature) {
+//     //return flattens the expression and the template literals insert the related variables into the return statement. 
+//     return (`${obj} are ${feature}`) 
+//   }
