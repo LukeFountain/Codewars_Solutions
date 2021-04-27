1e76a11885124be106d0d86f01d4f4e26c912dd8
Luke Fountain <stlfountain@gmail.com>
2021-04-27 13:38:07 -0700

    added daily challenge 007

---
+++ Daily_Challenge_007.js
@@ -0,0 +1,26 @@
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+
+
+// function isVow(a){
+
+//     //iterate through the array checking for the ascii code of lowercase vowels
+//     //if it contains a lowercase vowel replace the ascii code and return the whole array
+//     return(a.map(num => {
+//       return num === 97 ?  'a'
+//            : num === 101 ? 'e'
+//            : num === 105 ? 'i'
+//            : num === 111 ? 'o'
+//            : num === 117 ? 'u' 
+//            : num
+//     }
+                      
+//   ))
+    
+//    // 97 a, 101 e, 105 i , 111 o , 117 u, 
+    
+//   }
