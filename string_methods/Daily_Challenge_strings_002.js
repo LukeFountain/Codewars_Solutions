402d6827d719233d5f11eb5fec8ff6676a4253ae
Luke Fountain <stlfountain@gmail.com>
2021-06-09 15:14:53 -0700

    added some additional problems, mostly string methods.

---
+++ string_methods/Daily_Challenge_strings_002.js
@@ -0,0 +1,37 @@
+// Given a string S. You have to return another string such that even-indexed and odd-indexed characters of S are grouped and groups are space-separated (see sample below)
+
+// Note: 
+// 0 is considered to be an even index. 
+// All input strings are valid with no spaces
+// input: 'CodeWars'
+// output 'CdWr oeas'
+
+// S[0] = 'C'
+// S[1] = 'o'
+// S[2] = 'd'
+// S[3] = 'e'
+// S[4] = 'W'
+// S[5] = 'a'
+// S[6] = 'r'
+// S[7] = 's'
+// Even indices 0, 2, 4, 6, so we have 'CdWr' as the first group
+// odd ones are 1, 3, 5, 7, so the second group is 'oeas'
+// And the final string to return is 'Cdwr oeas'
+
+function sortMyString(S) {
+  
+    let evens = ''
+    let odds= ''
+     // you'll need to loop through the string 
+    for (i = 0; i < S.length; i++) {
+      if (i % 2 == 0){
+    // assigning based on even
+        evens+=S[i]
+      } else {
+    // assigning based on odd
+        odds+=S[i]
+      }
+    }
+    //concatinating the variables with a space between them
+  return(`${evens} ${odds}`)
+  }
